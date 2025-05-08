d4fc61c35b693f189ac715bfa79d3c5d9ac12a22
Sara-Rubaya <sararubaya4800@gmail.com>
2025-05-08 14:30:08 +0600

    Load News Data & Filter News Data based on Category

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
     const {id} =useParams();
-    console.log(id)
+    const data = useLoaderData();
+
+    const [CategoryNews, setCategoryNews] = useState([]);
+
+    // console.log(id, data);
+    useEffect(() => {
+        if(id == "0"){
+            setCategoryNews(data);
+            return;
+        }else if(id == "1"){
+            const filterNews = data.filter(news =>news.others.is_today_pick == true);
+            setCategoryNews(filterNews);
+
+        } else{
+            const filterNews = data.filter(news =>news.category_id == id);
+            console.log(filterNews);
+            setCategoryNews(filterNews);
+        }
+       
+
+        
+    },[data,id]);
+
+
     return (
         <div>
-            CategoryNews {id}
+           <h1>Total {CategoryNews.length} found</h1>
         </div>
     );
 };
