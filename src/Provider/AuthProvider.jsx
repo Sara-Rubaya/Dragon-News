850a4b38bc15a0474f7cbdce76c1cf2624ad5e0f
Sara-Rubaya <sararubaya4800@gmail.com>
2025-05-09 21:30:48 +0600

    added OnAuthStateChange

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,15 +1,33 @@
-import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import { app } from '../Firebase/Firebase.config';
 
 export const AuthContext = createContext();
 
+const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState({
-        name:"hablu mia",
-        email: "hablu@mia.com",
-    });
+    const [user, setUser] = useState(null);
+
+    console.log(user);
+
+    const createUser = (email, password) =>{
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    useEffect(()=>{
+   const unSubscribe = onAuthStateChanged(auth,(currentUser)=>{
+    setUser(currentUser);
+   });
+   return () =>{
+    
+    unSubscribe();
+
+   }
+    },[])
     const authData ={
         user,
         setUser,
+        createUser,
     }
     return <AuthContext value={authData}>{children}</AuthContext>;
     
